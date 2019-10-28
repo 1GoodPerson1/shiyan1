9978baf49170d39d9cb0521be830d57a154599bc
彬辉 李 <2533621490@qq.com>
2019-10-28 19:08:30 +0800

    添加项目文件。

---
+++ HPIT.Logistic.PM.DAL/UserDal.cs
@@ -0,0 +1,89 @@
+﻿using HPIT.Logistic.PM.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPIT.Logistic.PM.DAL
+{
+    public class UserDal
+    {
+        /// <summary>
+        /// 登录验证的方法
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="passWord"></param>
+        /// <returns></returns>
+        public object LoginIn(string userName, string passWord)
+        {
+            //与数据库操作的步骤
+            string sql = "select * from[LogisticsDB].[dbo].[User] where UserName=@username AND Password=@password ";
+            SqlParameter[] sqlParameters = new SqlParameter[2];
+            sqlParameters[0] = new SqlParameter("@username", userName);
+            sqlParameters[1] = new SqlParameter("@password", passWord);
+            //5.执行命令，返回结果
+            object result = DBHelper.ExcuteScalar(sql, sqlParameters);
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public UserModel GetUserById(string id)
+        {
+            UserModel model = new UserModel();
+            //根据用户id 查询用户信息
+            string sql = "select * from[LogisticsDB].[dbo].[User] where UserID=@ID ";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@ID", id);
+            //5.执行命令，返回结果
+            SqlDataReader reader = DBHelper.ExcuteSqlDataReader(sql, sqlParameters);
+            //判断有没有读到数据，hasRows 有没有行数据
+            if (reader.HasRows)
+            {
+                //读取第一条数据
+                while (reader.Read())
+                {
+                    model.UserName = reader["UserName"].ToString();
+                    model.PassWord = reader["Password"].ToString();
+                    model.UserID = int.Parse(reader["UserID"].ToString());
+                    model.CheckInTime = Convert.ToDateTime(reader["CheckInTime"]);
+                }
+            }
+            //返回结果
+            return model;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<UserModel> GetUserList()
+        {
+            string sql = "select * from[LogisticsDB].[dbo].[User]";
+            SqlDataReader reader = DBHelper.ExcuteSqlDataReader(sql);
+            //判断有没有读到数据，hasRows 有没有行数据
+            List<UserModel> users = new List<UserModel>();
+            if (reader.HasRows)
+            {
+                //读取第一条数据
+                while (reader.Read())
+                {
+                    UserModel model = new UserModel();
+                    model.UserName = reader["UserName"].ToString();
+                    model.PassWord = reader["Password"].ToString();
+                    model.Phone = reader["Phone"].ToString();
+                    model.Account = reader["Account"].ToString();
+                    model.UserID = int.Parse(reader["UserID"].ToString());
+                    model.CheckInTime = Convert.ToDateTime(reader["CheckInTime"]);
+                    users.Add(model);
+                }
+            }
+            return users;
+        }
+    }
+}
---
+++ HPIT.Logistic.PM.Model/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPIT.Logistic.PM.Model
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PassWord { get; set; }
+
+        public string Account { get; set; }
+
+        public string Phone { get; set; }
+
+
+        public DateTime CheckInTime { get; set; }
+    }
+}
